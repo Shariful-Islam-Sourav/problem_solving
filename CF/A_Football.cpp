a3771fcdd98186c6facdbe0700057236878480c3
unknown <sorabsorab347@gmail.com>
2025-02-24 20:00:38 +0900

    Initial Commit

---
+++ CF/A_Football.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<string> names(n);
+    for(int i = 0; i < n; i++) {
+        cin >> names[i];
+    }
+    vector<bool> counted(n, false);
+    int max{0};
+    string winner;
+    for(int i = 0; i < n; i++) {
+        if(counted[i]) continue;
+        int counter{1};
+        for(int j = i + 1; j < n; j++) {
+            if(names[i] == names[j]) {
+                counter++;
+                counted[j] = true;
+            }
+        }
+        if (counter > max) {
+            max = counter;
+            winner = names[i];
+        }
+    }
+    cout << winner << endl;
+    return 0;
+}
